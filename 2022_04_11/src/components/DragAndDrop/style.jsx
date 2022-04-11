a5237beda735f1faff7571c221934769b16c616d
JUNHEE LEE <54658162+junh0328@users.noreply.github.com>
2022-04-11 17:26:18 +0900

    init: 2022_04_11 repo

---
+++ 2022_04_11/src/components/DragAndDrop/style.jsx
@@ -0,0 +1,148 @@
+import styled from "styled-components";
+
+export const CustomForm = styled.form`
+  width: 96%;
+  padding: 2%;
+
+  user-select: none;
+`;
+
+export const ButtonContainer = styled.div`
+  button {
+    margin-right: 1%;
+  }
+`;
+
+export const DragContainer = styled.div`
+  margin-top: 1%;
+  .flex {
+    margin-bottom: 1%;
+    padding: 0.5%;
+    display: flex;
+    justify-content: flex-start;
+    border: 1px solid black;
+  }
+
+  span {
+    margin-right: 1%;
+  }
+
+  label {
+    width: 100%;
+    cursor: pointer;
+  }
+
+  .content {
+    width: 5%;
+  }
+
+  .id {
+    width: 2%;
+  }
+
+  .input {
+    margin-right: 1%;
+    border: none;
+    width: 5%;
+    cursor: default;
+    user-select: none;
+
+    :focus {
+      outline: none;
+    }
+  }
+
+  .remove {
+    margin-left: 2%;
+    color: red;
+
+    :hover {
+      cursor: pointer;
+      font-weight: bold;
+    }
+  }
+`;
+
+export const ScriptModal = styled.div`
+  display: ${(props) => (props.modalOn ? "flex" : "none")};
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.9);
+
+  cursor: auto;
+
+  z-index: 3;
+
+  input[type="text"] {
+    width: 100%;
+    border-bottom: 1px solid #fff;
+    background: none;
+    color: #fff;
+    font-size: 2vw;
+    font-weight: 400;
+  }
+
+  textarea {
+    margin-top: 1%;
+    margin-bottom: 0;
+    width: 100%;
+    height: 40vh;
+    border: 1px solid #fff;
+    overflow-y: auto;
+    background: none;
+    color: #fff;
+    font-size: 1.5vw;
+    font-weight: 400;
+
+    ::-webkit-scrollbar-thumb {
+      background: white;
+    }
+  }
+
+  button {
+    margin-top: 2%;
+    border: 1px solid #fff;
+    color: #fff;
+    font-size: 1.5vw;
+    font-weight: 400;
+  }
+
+  .exit {
+    position: absolute;
+    top: 2%;
+    right: 2%;
+    color: white;
+    cursor: pointer;
+
+    :hover {
+      color: #0064ff;
+    }
+  }
+
+  .save {
+    border: none;
+    background: inherit;
+    color: #fff;
+    font-weight: 400;
+    cursor: pointer;
+
+    :hover {
+      color: #0064ff;
+    }
+  }
+
+  .text_container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: center;
+    align-items: center;
+    margin: 0 auto;
+    width: 70%;
+  }
+`;
