0d88e801d1193f1dbfd1314abf51b913fa3f8aca
dlwnsgml0328 <junhee@eazel.net>
2022-05-16 16:42:54 +0900

    add: add minimize features using conditional rendering props

--- 2022_05_16/src/App.tsx
+++ 2022_05_16/src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
+  const [minimize, setMinimize] = useState<boolean>(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ background: '#000' }}>
+      <ModalWrapper minimize={minimize}>
+        <button onClick={() => setMinimize((prev) => !prev)}>minimize</button>
+        <div className='rect'>
+          <h3>rect</h3>
+        </div>
+      </ModalWrapper>
     </div>
   );
+};
+
+interface IModal {
+  minimize: boolean;
 }
+
+const ModalWrapper = styled.div<IModal>`
+  margin: 0 auto;
+  width: 100%;
+  height: 100vh;
+  user-select: none;
+  color: white;
+
+  ${({ minimize }) =>
+    minimize &&
+    `
+        .rect {
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          width: 50%;
+          height: 40vh;
+          background-color: salmon
+        }
+
+
+    `};
+
+  h3 {
+    margin: 0;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
 
 export default App;
